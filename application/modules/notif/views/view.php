c25dd52d869bc4fbfb03116fb2aadf0d565b7696
yogigan <bachtiarnuryogipratama1@gmail.com>
2020-06-09 20:21:31 +0700

    notifikasi

---
+++ application/modules/notif/views/view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Membuat Pesan Alert</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container">
+<h1 class="mt-4 mb-4">Membuat Pesan Alert</h1>
+<div class="alert alert-success fade show">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+Pesan Alert Sukses
+</div>
+<div class="alert alert-info fade show">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+Pesan Alert Info
+</div>
+<div class="alert alert-warning fade show">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+Pesan Alert Peringatan
+</div>
+<div class="alert alert-danger fade show">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+Pesan Alert Gagal
+</div>
+</div>
+</div>
+</body>
+</html>
